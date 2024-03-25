b51bd53a6e7bde707f9cf551cca24ad0a617c014
SachithRKA <sachithRKA@gmail.com>
2024-03-25 19:56:56 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 'use client'
 
-import HomePage from "./pages/Home"
-import LoginPage from './pages/Login';
-import SignupPage from './pages/Signup';
-import PrizingPage from './pages/Prizing';
-import Index from './pages/index';
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Index from "./pages/index"
 
 function Page() {
   return (
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import HomePage from './Home';
+import LoginPage from './Login';
+import SignupPage from './Signup';
+import PrizingPage from './Prizing';
 
 export default function Index() {
-  return (
-    <div>
-        <a href={`${process.env.NEXT_PUBLIC_BASE_PATH}/Testing`}>Go to Testing Page</a>        
-        <h1>HELLO WORLD LINKS WORLS HOPE SO</h1>
-    </div>
-  );
+    return (
+        <div>
+        <HomePage />
+        <LoginPage />
+        <SignupPage />
+        <PrizingPage />
+        </div>
+    );
 }
