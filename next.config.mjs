8225f2cdd5bee080b84588baa3f52314f654b6e5
SachithRKA <sachithRKA@gmail.com>
2024-03-25 19:53:07 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/ReactTypeScriptFrontendTemplate",
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/",
   
     /**
      * Disable server-based image optimization. Next.js does not support
