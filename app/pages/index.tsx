eb74ef1ac2dc598eb2d31a2e894e02a73831d1dd
SachithRKA <sachithRKA@gmail.com>
2024-03-25 19:27:29 -0400

    update

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Index() {
   return (
-    <>
-      <a href="/Testing">Go to Testing Page</a>
-      <h1>HELLO WORLD LINKS WORLS HOPE SO</h1>
-    </>
+    <div>
+        <a href="/Testing">Go to Testing Page</a>
+        <h1>HELLO WORLD LINKS WORLS HOPE SO</h1>
+    </div>
   );
 }
