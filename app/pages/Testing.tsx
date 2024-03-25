0d4fcd1112a5a40401af6afea176437adbe96f06
SachithRKA <sachithRKA@gmail.com>
2024-03-25 18:31:21 -0400

    update

--- app/pages/Testing.tsx
+++ app/pages/Testing.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import HomePage from './Home';
-import Signup from './Signup';
-import Header from '../components/Header';
-import Link from 'next/link';
 
 export default function Testing() {
-    
-    return <>
-            <Link href="/"><a>Testing</a></Link>
-        <h1>HELLO WORLD LINKS WORLS HOPE SO</h1>
-    </>;
+  return (
+    <div>
+      <h1>Testing Page</h1>
+    </div>
+  );
 }
