c8dbac92c6e7b8c8c24d030734a88850cf2f855e
SachithRKA <sachithRKA@gmail.com>
2024-03-25 15:50:18 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,37 +4,13 @@ import HomePage from "./pages/Home"
 import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
 import PrizingPage from './pages/Prizing';
+import Index from './pages/index';
 import React from "react";
 import Link from "next/link";
 
 function Page() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/Home">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/Login">
-              <a>Login</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/Signup">
-              <a>Signup</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/Prizing">
-              <a>Prizing</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Index />
   );
 }
 
