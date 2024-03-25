351a1fcc8f6c8ce4f0a4be2c6ab939087b28ea58
SachithRKA <sachithRKA@gmail.com>
2024-03-25 18:18:40 -0400

    update

--- app/pages/Testing.tsx
+++ app/pages/Testing.tsx
@@ -3,11 +3,11 @@ import HomePage from './Home';
 import Signup from './Signup';
 import Header from '../components/Header';
 import Link from 'next/link';
-export default function Index() {
+
+export default function Testing() {
     
     return <>
-        <Header />
-        <Link href="/Testing"><a>Testing</a></Link>
+            <Link href="/"><a>Testing</a></Link>
         <h1>HELLO WORLD LINKS WORLS HOPE SO</h1>
     </>;
 }
