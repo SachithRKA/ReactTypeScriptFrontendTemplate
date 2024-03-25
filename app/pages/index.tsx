2479c2496eafe7a57aa6837e998347fe67e437bf
SachithRKA <sachithRKA@gmail.com>
2024-03-25 19:24:06 -0400

    update

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import Router from 'next/router';
 
 export default function Index() {
-  const goToTestingPage = () => {
-    Router.push('/Testing');
-  };
-
   return (
     <>
-      <button onClick={goToTestingPage}>Go to Testing Page</button>
+      <a href="/Testing">Go to Testing Page</a>
       <h1>HELLO WORLD LINKS WORLS HOPE SO</h1>
     </>
   );
